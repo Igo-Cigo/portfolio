634f4ad792bb3cb2e6eb29eed0c8cfe04b812fc7
Igo-Cigo <igocigonogedigo@gmail.com>
2022-04-21 20:54:18 +0200

    Added React Bootsrap and React Router v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Welcome from "./Welcome";
 import Websites from "./Websites";
+import Home from "./Home";
+import Contact from "./Contact";
+import Navigation from "./Navigation";
 
 function App() {
   return (
     <div className="container">
+      <Navigation />
       <Welcome />
-      <nav>
-        <p>
-          <Link to="/">Home</Link>
-        </p>
-        <p>
-          <Link to="/websites">Websites</Link>
-        </p>
-      </nav>
+      <hr/>
       <Routes>
-        <Route path="/" element={<Websites />} />
+        <Route path="/" element={<Home />} />
         <Route path="/websites" element={<Websites />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <img className="pt-3 center" src="/assets/images/moon.jpeg" alt="moon"></img>
     </div>
   );
 }
